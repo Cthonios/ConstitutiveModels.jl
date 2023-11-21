6f89374f290d1097c54693d6be643c4e82bf8ccd
Craig M. Hamel <cmhamel32@gmail.com>
2023-11-21 16:42:12 -0500

    removing utils stuff for now and mostly finished patching up Motions.jl. We still always use Tensors.jl in the back end regardless of the requested type.

--- src/Motions.jl
+++ src/Motions.jl
@@ -10,8 +10,6 @@ Returns the deformation gradient for a given motion
 function deformation_gradient end
 function motion_objective end
 
-
-
 @doc raw"""
 Provides an analytic motion for uniaxial stress
 assuming perfect incompressibility.
@@ -39,9 +37,6 @@ SMatrix{3, 3, T, 9}((λ, 0., 0., 0., 1. / sqrt(λ), 0., 0., 0., 1. / sqrt(λ)))
 deformation_gradient(::Type{IsochoricUniaxialStress}, λ::T, type::Type{<:Matrix}) where T <: Number = 
 T[λ 0. 0.; 0. 1. / sqrt(λ) 0.; 0. 0. 1. / sqrt(λ)]
 
-# deformation_gradient(motion::Type{IsochoricUniaxialStress}, λ::T; type::Type = DefaultMotionType) where T <: Number = 
-# deformation_gradient(motion, λ, type)
-
 @doc raw"""
 Provides an analytic motion for simple shear.
 
@@ -67,9 +62,6 @@ deformation_gradient(::Type{SimpleShear}, γ::T, ::Type{<:SMatrix}) where T <: N
 SMatrix{3, 3, T, 9}((1., 0., 0., γ, 1., 0., 0., 0., 1.))
 deformation_gradient(::Type{SimpleShear}, γ::T, type::Type{<:Matrix}) where T <: Number = 
 T[1. γ 0.; 0. 1. 0.; 0. 0. 1.]
-
-# deformation_gradient(motion::Type{SimpleShear}, γ::T; type::Type = DefaultMotionType) where T <: Number = 
-# deformation_gradient(motion, γ, type)
 
 @doc raw"""
 Provides an analytic motion for uniaxial strain
@@ -97,15 +89,12 @@ SMatrix{3, 3, T, 9}((λ, 0., 0., 0., 1., 0., 0., 0., 1.))
 deformation_gradient(::Type{UniaxialStrain}, λ::T, type::Type{<:Matrix}) where T <: Number = 
 T[λ 0. 0.; 0. 1. 0.; 0. 0. 1.]
 
-# deformation_gradient(motion::Type{}, γ::T; type::Type = DefaultMotionType) where T <: Number = 
-# deformation_gradient(motion, γ, type)
-
 struct UniaxialStressDisplacementControl <: SimpleMotion
 end
 
 function form_deformation_gradient(
   ::Type{UniaxialStressDisplacementControl},
-  λ::T, x::V
+  λ::T, x::V, ::Type{<:Tensor}
 ) where {T <: Number, V <: AbstractArray}
   F = Tensor{2, 3, eltype(x), 9}((
     λ,    x[8], x[7],
@@ -115,12 +104,51 @@ function form_deformation_gradient(
   return F
 end
 
+function form_deformation_gradient(
+  ::Type{UniaxialStressDisplacementControl},
+  λ::T, x::V, type::Type{MMatrix}
+) where {T <: Number, V <: AbstractArray}
+  F = MMatrix{3, 3, eltype(x), 9}((
+    λ,    x[8], x[7],
+    x[5], x[1], x[6],
+    x[4], x[3], x[2]
+  ))
+  return F
+end
+
+function form_deformation_gradient(
+  ::Type{UniaxialStressDisplacementControl},
+  λ::T, x::V, type::Type{SMatrix}
+) where {T <: Number, V <: AbstractArray}
+  F = SMatrix{3, 3, eltype(x), 9}((
+    λ,    x[8], x[7],
+    x[5], x[1], x[6],
+    x[4], x[3], x[2]
+  ))
+  return F
+end
+
+function form_deformation_gradient(
+  ::Type{UniaxialStressDisplacementControl},
+  λ::T, x::V, type::Type{Matrix}
+) where {T <: Number, V <: AbstractArray}
+  F = T[
+    λ    x[8] x[7];
+    x[5] x[1] x[6];
+    x[4] x[3] x[2]
+  ]
+  return F
+end
+
+# TODO need to specialize to regular ole matrices
+# with form_deformation_gradient
+
 function motion_objective(
   motion::Type{UniaxialStressDisplacementControl}, 
-  model::Mod, props::Props, state,
+  model::Mod, props::Props, state, type::Type,
   u, p
 ) where {Mod <: ConstitutiveModel, Props <: AbstractArray}
-  F        = form_deformation_gradient(motion, p[1], u)
+  F        = form_deformation_gradient(motion, p[1], u, type)
   P, state = pk1_stress(model, props, F, state)
   Pvec     = @views tovoigt(SVector, P)[2:end]
   return Pvec
@@ -130,14 +158,27 @@ end
 """
 function deformation_gradient(
   motion::Type{UniaxialStressDisplacementControl},
-  model::Mod, props::Props, state, λ::T,
+  model::Mod, props::Props, state, λ::T, type::Type
 ) where {T <: Number, Mod <: ConstitutiveModel, Props <: AbstractArray{T, 1}}
 
+  # system seems singular for identity so this is a catch for now
+  if λ ≈ 1.
+    if type <: Tensor
+      return one(SymmetricTensor{2, 3, T, 6})
+    elseif type <: MMatrix
+      return MMatrix{3, 3, T, 9}((1., 0., 0., 0., 1., 0., 0., 0., 1.))
+    elseif type <: SMatrix
+      return SMatrix{3, 3, T, 9}((1., 0., 0., 0., 1., 0., 0., 0., 1.))
+    elseif type <: Matrix
+      return T[1. 0. 0.; 0. 1. 0.; 0. 0. 1.]
+    end
+  end
+
   x0      = SVector{8, T}([1., 1., 0., 0., 0., 0., 0., 0.])
-  f       = (u, p) -> motion_objective(motion, model, props, state, u, p)
+  f       = (u, p) -> motion_objective(motion, model, props, state, Tensor, u, p)
   problem = NonlinearProblem(f, x0, SVector{1, Float64}([λ]))
   sol     = solve(problem, NewtonRaphson())
-  F       = form_deformation_gradient(motion, λ, sol.u)
+  F       = form_deformation_gradient(motion, λ, sol.u, type)
   return F
 end
 
@@ -146,3 +187,5 @@ end
 const DefaultMotionType = MMatrix
 deformation_gradient(motion::Type{<:SimpleMotion}, λ::T; type::Type = DefaultMotionType) where T <: Number = 
 deformation_gradient(motion, λ, type)
+deformation_gradient(motion::Type{<:SimpleMotion}, model::Mod, props::Props, state, λ::T; type::Type = DefaultMotionType) where {Mod <: MechanicalModel, Props <: AbstractArray, T <: Number} =
+deformation_gradient(motion, model, props, state, λ, type)
